4aa214de636f37eb232106043ce58cc6509a8228
legend797 <legendneverdie797@gmail.com>
2024-02-28 17:47:34 +0000

    fix home collab photo commit

--- live-app/src/app/components/Collab.js
+++ live-app/src/app/components/Collab.js
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
-import { Image } from '@chakra-ui/react';
+import Image from "next/image";
+// import { Image } from '@chakra-ui/react';
 
 
 const Collab = ({src}) => {
